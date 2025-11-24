91394731ea1dd7226045e3d20ad45b52a8636209
Aylin Aydın Topcan <aylin@Aylin-MacBook-Pro.local>
2025-11-24 22:31:55 +0300

    scripts added

--- dev/check.sh
+++ dev/check.sh
@@ -3,6 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=40G
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
 
 source /opt/python3/venv/base/bin/activate
 python3 -c 'import pandas; print("Pandas Version:", pandas.__version__)'
